d742ea6e4a210b8db16a2578756b0ae5e769d24c
bagas12dwi <bag12dwi.s@gmail.com>
2021-10-19 11:02:02 +0700

    Proses pengerjaan transaksi

---
+++ src/Main/Penarikan.java
@@ -0,0 +1,14 @@
+package Main;
+
+public class Penarikan extends Transaksi{
+    public static void tarik(){
+        int[] nominal = {50000, 100000, 200000, 300000, 500000, 1000000};
+        System.out.println("1. 50.000 \t4. 300.000");
+        System.out.println("2. 100.000 \t5. 500.000");
+        System.out.println("3. 200.000 \t6. 1.000.000");
+        System.out.println("Pilih jumlah penarikan tunai yang anda ingin kan : ");
+        tarik = input.nextInt();
+
+
+    }
+}
--- src/Main/Transaksi.java
+++ src/Main/Transaksi.java
@@ -1,13 +1,27 @@
 package Main;
 
+import java.util.Scanner;
+
 public class Transaksi {
-    public static int noRek, jenisBayar;
+    public static Scanner input = new Scanner(System.in);
+    public static String noRek, jenisBayar;
     public static int pilihan, tarik, bayar, bank;
+    public static int saldo = 30000000;
 
     public static void transaksi() {
         System.out.println("\n\tBank Maju Sejahtera\n\t");
         System.out.println("\t Pilih Jenis Transaksi");
         System.out.println("1. Penarikan \t 3. Pembayaran");
         System.out.println("2. Transfer \t 4. Info Rekening");
+        pilihan = input.nextInt();
+
+        switch (pilihan){
+            case 1:
+                System.out.println("\nPENARIKAN");
+
+                break;
+
+            case 2:
+        }
     }
 }
