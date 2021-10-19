a6ac04037a80da904809994f4ef203e7b3380ad9
bagas12dwi <bag12dwi.s@gmail.com>
2021-10-19 10:16:12 +0700

    menambahkan class transaksi

---
+++ src/Main/Main.java
@@ -0,0 +1,29 @@
+package Main;
+
+import java.util.*;
+
+public class Main {
+    public static Scanner input = new Scanner(System.in);
+    public static String pin, Try;
+
+
+    public static void main(String[] args){
+        Transaksi transaksi = new Transaksi();
+
+        System.out.println("\t Bank Maju Sejahtera \t");
+        System.out.println("Masukkan PIN (tekan Enter ketika selesai memasukan PIN)");
+        pin = input.nextLine();
+
+        if (pin.equals("170845")){
+            Try = "";
+            do {
+                transaksi.transaksi();
+                System.out.println("\n Apakah anda ingin mencoba lagi? Y/N");
+                Try = input.nextLine();
+            }while (Try.equalsIgnoreCase("y"));
+        } else {
+            System.out.println("Maaf, nomor PIN anda salah!");
+        }
+    }
+
+}
---
+++ src/Main/Transaksi.java
@@ -0,0 +1,13 @@
+package Main;
+
+public class Transaksi {
+    public static int noRek, jenisBayar;
+    public static int pilihan, tarik, bayar, bank;
+
+    public static void transaksi() {
+        System.out.println("\n\tBank Maju Sejahtera\n\t");
+        System.out.println("\t Pilih Jenis Transaksi");
+        System.out.println("1. Penarikan \t 3. Pembayaran");
+        System.out.println("2. Transfer \t 4. Info Rekening");
+    }
+}
